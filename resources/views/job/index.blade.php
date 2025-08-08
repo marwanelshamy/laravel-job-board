5f341964ecf3017d42fdbbb47b7c3f8ee6d954b0
Marawan Elshamy <marawanelshamy8@gmail.com>
2025-08-08 16:45:48 +0300

    new

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,9 +1,16 @@
-<div>
-   <h1>job board</h1>
-
-   @foreach ($jobs as $job)
-     <div>
-        {{$job['title']  }} : {{ $job['salary'] }}
-     </div>
-   @endforeach
-</div>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+    </head>
+    <body>
+      <h1>job board</h1>
+     <nav>
+        <a href="/">home</a>
+        <a href="/about">about</a>
+        <a href="/contact">contact</a>
+     </nav>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\IndexConrtoller;
 use App\Http\Controllers\JobControler;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [IndexConrtoller::class, 'index' ]);
 
 Route::get('/job',[JobControler::class,'index']);
